eddfcb49d0880faa42b32733c7d0f46e82d05e05
KenebiObi <kenebiobi@gmail.com>
2024-03-10 16:15:02 +0100

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class PasswordApp extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return HomePage();
+            return const HomePage();
           } else {
             return MainScreen();
           }
